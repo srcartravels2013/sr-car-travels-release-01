52ecde1beb4d7e6c45b968a0803bd8627a26e3fa
srcartravels2013 <srcartravels2013@gmail.com>
2023-09-10 23:35:40 +0530

    added configuration in main class
    added configuration in main class

--- src/main/java/com/sr/travels/main/SrTravelsMainApplication.java
+++ src/main/java/com/sr/travels/main/SrTravelsMainApplication.java
@@ -2,14 +2,21 @@ package com.sr.travels.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
 @ComponentScan("com.sr.travels")
-public class SrTravelsMainApplication {
+public class SrTravelsMainApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SrTravelsMainApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SrTravelsMainApplication.class);
 	}
 
 }
